fa32a91b6ba61152b4bb653966e3f985eb87c66e
jiaxin sun <jiaxinsun2023@gmail.com>
2024-11-14 15:44:33 -0500

    commit

--- gamePgae.js
+++ gamePgae.js
@@ -1,3 +1,11 @@
+// 1.press start button--fucntion:gameStart
+    // A.random sequence (item:1\2\3)(difficulty:3-10)(1-triangle，2-square,3-circle)
+    // B.
+// 2.
+// 
+// 
+// 
+
 const shapes = document.querySelectorAll('.triangle, .square, .circle');
 // store thses html element in the VARIABLE SHAPES(NodeList)
 
@@ -7,14 +15,17 @@ let level =3; // initial difficulty
 // let currentIndex = 0;
 
 
+// for test now ，need to edit
 function startGame() {
+    console.log('游戏开始了！');    
     message.textContent = "你点击了按钮！";
-    console.log('游戏开始了！');
+   
 }
 
 
 // startButton
 const startButton = document.querySelector('.startButton');
 startButton.addEventListener('click', startGame); // when u click START,call the startGame function 
+
 const message = document.getElementById('message');
 
